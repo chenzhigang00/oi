30b4b8a84be3272d658fbee96397b8d7798b5670
chenzhigang00 <1312383034@qq.com>
2025-03-08 17:56:53 +0800

    团建v2

--- 团建.cpp
+++ 团建.cpp
@@ -1,81 +1,59 @@
 #include <iostream>
-#include <cstring>
 #include <algorithm>
-#include <unordered_map>
-
+#include <cstring>
+#include <vector>
+#include <map>
 using namespace std;
 
-const int N=200010,M=N*2;
-
-// head, edge, next edge, index
-int h[2][N], e[2][M], ne[2][M], idx;    // 用于存储两棵树的邻接表
-int w1[N],w2[N];    // 分别存储两棵树各个结点的权值
-int n,m;            // 结点数，边数
-
-// 给第 i 棵树添加一条边 a->b
-void add(int i, int a, int b){
-    e[i][idx] = b;
-    ne[i][idx] = h[i][a];
-    h[i][a] = idx++;
-}
-
-// 深度优先搜索函数
-int dfs(int u1, int u2, int fa1, int fa2){
-    int res = 0;
-    unordered_map<int, int> hs;
-
-    // 遍历第一棵树的子结点
-    for(int i = h[0][u1]; i != -1; i = ne[0][i]){
-        int j = e[0][i];
-        if (j == fa1) 
-            continue;
-        hs[w1[j]] = j;
-    }
-
-    // 遍历第二棵树的子结点
-    for (int i = h[1][u2]; ~i; i = ne[1][i]){
-        int j = e[1][i];
-        if (j == fa2)
-            continue;
-
-        // 如果存在权值相同的结点，就能够继续向下搜索
-        if (hs.count(w2[j])){
-            // 计算以这两个结点为起点往下的最长公共路径长度
-            res = max(res, dfs(hs[w2[j]], j, u1, u2));
-        }
-    }
+const int N = 2e5+10;
+int n, s, a[N], b[N], sum, m, dmax, _, __;
+vector<int> atir[N];
+vector<int> btir[N];
+bool sta[N], visb[N];
+void dfs(int u, int d, int w){
+    sta[u] = true;
+    visb[w] = true;
+    dmax = max(dmax, d);
+    map<int,int> bk;
+    for (int i:atir[u])
+        if (!sta[i])
+            bk[a[i]] = i;
     
-    // 返回的结果需要加上当前结点
-    return res + 1;
+    for (int j:btir[w])
+        if (!visb[j] && bk[b[j]])
+            dfs(bk[b[j]],d+1,j);
+    // for(int i: atir[u]){
+    //     for(int j:btir[w]){
+    //         if(a[i] == b[j] && !sta[i] && !visb[j]){
+    //             dfs(i,d+1,j);
+    //         }
+    //     }
+    // }
 }
 
 int main(){
-    scanf("%d%d", &n,&m);
-    memset(h,-1,sizeof(h));
-    for (int i = 1; i<=n; i ++) scanf("%d", &w1[i]);
-    for (int i = 1; i<=m; i ++) scanf("%d", &w2[i]);
-
-    for (int i = 0; i < n - 1; i ++){
-        int a, b;
-        scanf("%d%d", &a, &b);
-        add(0,a,b);
-        add(0,b,a);
+    cin >> n >> m;
+    for (int i = 1; i<=n;i++){
+        cin >> a[i];
+    }
+    for (int i = 1; i<=m;i++){
+        cin >> b[i];
     }
 
-    for (int i = 0; i < m - 1; i ++){
-        int a, b;
-        scanf("%d%d", &a, &b);
-        add(1,a,b);
-        add(1,b,a);
+    for (int i = 1; i <n;i++){
+        cin >> _ >> __;
+        atir[_].push_back(__);
+        atir[__].push_back(_);
     }
-
-    int res = 0;
-    if (w1[1]==w2[1]){
-        res = dfs(1,1,0,0);
+    for(int i = 1; i<m;i++){
+        cin >> _ >> __;
+        btir[_].push_back(__);
+        btir[__].push_back(_);
     }
-    printf("%d\n", res);
-
+    if (a[1]==b[1]){
+        dfs(1,1,1);
+    }
+    cout << dmax << endl;
     return 0;
 }
-
 
