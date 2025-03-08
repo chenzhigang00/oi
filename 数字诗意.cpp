a1f17744377227431fb8074be3d2645512590abd
chenzhigang00 <1312383034@qq.com>
2025-03-08 18:24:44 +0800

    数字诗意v2

--- 数字诗意.cpp
+++ 数字诗意.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
-#include <set>
+//#include <set>
+#include<cmath>
 
 using namespace std;
 typedef long long LL;
 
 int n;
-set<LL> S;
+//set<LL> S;
 
 int main(){
-    LL t = 1;
-    for (int i = 1; i< 63; ++i)
-        S.insert(t), t *= 2;
+    // LL t = 1;
+    // for (int i = 1; i< 63; ++i)
+    //     S.insert(t), t *= 2;
     
     cin >> n;
-
     int res = 0;
     while(n--){
         LL x;
         cin >> x;
-        res += S.count(x);
+        LL p = log2(x);
+        
+        if (1ll<<p == x)
+            res += 1;
     }
 
     cout <<res <<endl;
